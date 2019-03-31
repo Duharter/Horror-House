4263a878d7687b4b0c7a4fce649f47ff13e3e1c3
Janice <janiceng@ufl.edu>
2019-03-31 02:33:03 -0400

    Interaction
    Added UI interact
    Added onkeypresses

--- Horror House/Assets/Scripts/InteractOnTrigger.cs
+++ Horror House/Assets/Scripts/InteractOnTrigger.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractOnTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    Text message;
+    bool triggerable;
+    void Start() {
+        message = GameObject.Find("msg_interact").GetComponent<Text>(); 
+    }
+    void OnTriggerEnter (Collider col) {
+        if (col.gameObject.name == "Player")
+        {
+            message.enabled = true;
+            triggerable = true;
+        }
+    }
+    void OnTriggerExit() {
+        message.enabled = false;
+        triggerable = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (triggerable) {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Interacting");
+            }
+        }
     }
 }
