d411e74ea7d3dedf955b3ae1bad108f2950e1436
Janice <janiceng@ufl.edu>
2019-04-02 15:01:54 -0400

    Credits + Title
    Fixed HUD and GUI

---
+++ Horror House/Assets/Scripts/MenuActions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void ReturnTitle() {
+        SceneManager.LoadScene(0);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
