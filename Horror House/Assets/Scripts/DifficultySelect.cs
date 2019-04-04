0b7717ad75decf8cafcc9c70006835e37f34ab0d
Janice <janiceng@ufl.edu>
2019-04-04 19:01:02 -0400

    Changing difficulty setting

---
+++ Horror House/Assets/Scripts/DifficultySelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultySelect : MonoBehaviour
+{
+    public GameObject data;
+
+    public void SetEasy() {
+        DifficultySetting.difficulty = 1;
+    }
+    public void SetMedium() {
+        DifficultySetting.difficulty = 2;
+    }
+    public void SetHard() {
+        DifficultySetting.difficulty = 3;
+    }
+}
---
+++ Horror House/Assets/Scripts/DifficultySetting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultySetting : MonoBehaviour
+{
+    
+    static public int difficulty;
+
+    void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update() {
+        Debug.Log(difficulty);
+    }
+
+   
+}
